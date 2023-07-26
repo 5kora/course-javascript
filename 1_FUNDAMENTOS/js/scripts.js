f908403169f8e80328f7256d8e5776b59f3505bf
Marcos <araujo678@gmail.com>
2023-07-26 18:10:17 -0300

    Exercicio 5

--- 1_FUNDAMENTOS/js/scripts.js
+++ 1_FUNDAMENTOS/js/scripts.js
@@ -32,7 +32,46 @@ const concatString = (nome, idade, cidade) => {
 
 const stringConcatenada = concatString(nome, idade, cidade);
 
-console.log(`${celsius} graus celsius equivalem a ${celsiusToFahrenheit(celsius)} graus Fahrenheit!`);
-console.log(`${fahrenheit} graus Fahrenheit equivalem a ${fahrenheitToCelsius(fahrenheit)} graus celsius!`);
+// 4 - Calculo de área e perimetro
+const comprimento = 10;
+const largura = 5;
+
+const areaRetangulo = (comprimento, largura) => {
+    return (comprimento * largura).toFixed(2);
+}
+
+const resultadoAreaRetangulo = areaRetangulo(comprimento, largura);
+
+const perimetroRetangulo = (comprimento, largura) => {
+    return  2 * (largura + comprimento).toFixed(2);
+}
+
+const resultadoPerimetroRetangulo = perimetroRetangulo(comprimento, largura);
+
+// 5 - Verificação de divisibilidade
+const numeroA = 10;
+const numeroB = 2;
+const numeroC = 3;
+
+const verificaDivisibilidade = (numeroA, numeroB) => {
+    return numeroA % numeroB === 0;
+}
+
+const resultadoAB = verificaDivisibilidade(numeroA, numeroB);
+const resultadoAC = verificaDivisibilidade(numeroA, numeroC);
+
+// Atividade 1
+console.log(`${celsius} graus celsius equivalem a ${celsiusToFahrenheit(celsius)} graus Fahrenheit! \n ${fahrenheit} graus Fahrenheit equivalem a ${fahrenheitToCelsius(fahrenheit)} graus celsius!`);
+
+
+// Atividade 2
 console.log(`O IMC para a altura ${altura} e o peso ${peso} é de: ${IMC}`);
+
+// Atividade 3
 console.log(`${stringConcatenada}`);
+
+// Atividade 4
+console.log(`o valor da area do retangulo é: ${resultadoAreaRetangulo} e o primetro do retangulo é: o valor da area do retangulo é: ${resultadoPerimetroRetangulo}`);
+
+// Atividade 5
+console.log(`${numeroA} / ${numeroB} é divisivel?  ${resultadoAB ? "Sim!" : "Não!"} ja o ${numeroA} / ${numeroC} é divisivel?  ${resultadoAC ? "Sim!" : "Não!"}`);
