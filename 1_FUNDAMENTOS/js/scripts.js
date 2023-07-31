beb7c2030571a8837570ab5b09fa5a2e60a5a427
Marcos <araujo678@gmail.com>
2023-07-31 18:10:23 -0300

    Atividade 19

--- 1_FUNDAMENTOS/js/scripts.js
+++ 1_FUNDAMENTOS/js/scripts.js
@@ -219,6 +219,65 @@ const calcFibonacci = (num) => {
 
 const resultFibonacci = calcFibonacci(seletorFibonacci);
 
+// 15 - Par ou Impar
+
+const evenOrOdd = (num) => {
+    if (num % 2 === 0) {
+        return `O numero ${num} é par!`
+    } else {
+        return `O numero ${num} é impar!`
+    }
+}
+
+//16 - Fatorial
+
+const calcFactorial = (num) => {
+    if (num === 0 || num === 1){
+        return 1;
+    }else{
+        return num * calcFactorial(num - 1)
+    }
+}
+
+//17 - Min e max
+
+const maxValue = (num1, num2) => {
+    if(num1 > num2){
+        return `O maior valor entre ${num1} e ${num2} é: ${num1}`;
+    }else{
+        return `O maior valor entre ${num1} e ${num2} é: ${num2}`;
+    }
+}
+
+const minValue = (num1, num2) => {
+    if(num1 > num2){
+        return `O menor valor entre ${num1} e ${num2} é: ${num2}`;
+    }else{
+        return `O menor valor entre ${num1} e ${num2} é: ${num1}`;
+    }
+}
+
+// 18 - inversão de string
+
+const stringRevert = (string) => {
+    return string.split("").reverse().join("");
+}
+
+// 19 - Contagem de vogais
+
+const vowelCounter = (string) => {
+    let vowel = "AEIOU";
+    let stringSplitted = string.toUpperCase().split("");
+    let counter = 0;
+    stringSplitted.forEach(letter => {
+        if(vowel.includes(letter)){
+            counter++;
+        }
+    });
+
+    return `A palavra ${string} contem ${counter} vogais`;
+}
+
 //-----------------------------------------------------------------------------------------------------------------------------------------------
 
 // Atividade 1
@@ -252,14 +311,31 @@ console.log(`O resultado idade = 50 e estudante é ${calcTarifa(50, true)} \n O 
 console.log(`O somatorio de 3 é: ${somatioN(3)} \n somatorio de 10 é: ${somatioN(10)}`);
 
 // Atividade 11
-console.log(`${tabuada(baseTabuada)}`)
+console.log(`${tabuada(baseTabuada)}`);
 
 // Atividade 12
-primeNumberList(baseNumeroPrimo)
+primeNumberList(baseNumeroPrimo);
 
 // Atividade 13
-console.log(printResult)
+console.log(printResult);
 
 // Atividade 14
-console.log(resultFibonacci)
+console.log(resultFibonacci);
 
+// Atividade 15
+console.log(evenOrOdd(4));
+console.log(evenOrOdd(9));
+
+// Atividade 16
+console.log(calcFactorial(4));
+console.log(calcFactorial(3));
+
+// Atividade 17
+console.log(maxValue(4, 8));
+console.log(minValue(4, 8));
+
+// Atividade 18
+console.log(stringRevert("Dollynho"));
+
+// Atividade 19
+console.log(vowelCounter("Dollynho"));
