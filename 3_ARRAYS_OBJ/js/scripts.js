0159e2e3db951dede5adcfb313a20742cea031a0
Marcos <araujo678@gmail.com>
2023-08-07 13:49:05 -0300

    Atividade 28

--- 3_ARRAYS_OBJ/js/scripts.js
+++ 3_ARRAYS_OBJ/js/scripts.js
@@ -1,12 +1,12 @@
 
 //23 - Elementos duplicados
-const removeRepeated = (array) =>{
+const removeRepeated = (array) => {
     return Array.from(new Set(array));
 }
 
 // 24 - Concat Arrays
 const concatArrays = (array1, array2) => {
-return array1.concat(array2);
+    return array1.concat(array2);
 }
 
 // 25 - Interseção
@@ -19,13 +19,52 @@ const intersectionArrays = (array1, array2) => {
 // 26 - Media dos elementos
 
 const arrayAverage = (array) => {
-    let sum = 0
-    array.forEach(element => {
-        return sum +=element;
-    });
+    let sum = array.reduce((acumulador, value) => acumulador + value, 0);
     return (sum / array.length).toFixed(2);
 }
 
+//27 - Calcular a soma de propriedades
+const objetExample = {
+    a: 10,
+    b: 20,
+    c: 30
+}
+
+const sumProperties = (obj) => {
+    let sum = 0;
+
+    for (const prop in obj) {
+        if (obj.hasOwnProperty(prop) && typeof obj[prop] === "number") {
+            sum += obj[prop];
+        }
+    }
+
+    return sum;
+}
+
+//28 - Filtrando propriedades
+const completeObj = {
+    nome: 'Dollynho',
+    idade: 36,
+    profissao: "refrigerante",
+    sabor: 'Guarana',
+    top: 'nao'
+}
+
+const filterArray = ["nome", "profissao", "sabor"];
+
+const filterProperties = (filterArray, obj) => {
+    let newObject = {};
+
+    filterArray.forEach(prop => {
+        if(obj.hasOwnProperty(prop)){
+            newObject[prop] = obj[prop];
+        }
+        return 
+    });
+
+    return newObject;
+}
 //-----------------------------------------------------------------------------------------------------------------------------------------------
 
 // Atividade 23
@@ -35,18 +74,26 @@ const notRepeatedArray = removeRepeated(repeatedArray)
 console.log(`O array original é ${repeatedArray} após o filtro ficou: ${notRepeatedArray}`)
 
 // Atividade 24
-const array1 = [1,2,3];
-const array2 = [4,5,6];
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
 
 console.log(`O array 1 é ${array1}, o array 2 é ${array2} após o concat ficou: ${concatArrays(array1, array2)}`)
 
 // Atividade 25
-const intersectionArray1 = [1,2,3,4,5];
-const intersectionArray2 = [4,5,6];
+const intersectionArray1 = [1, 2, 3, 4, 5];
+const intersectionArray2 = [4, 5, 6];
 const intersectionResult = intersectionArrays(intersectionArray1, intersectionArray2)
 console.log(`O array 1 é ${intersectionArray1}, o array 2 é ${intersectionArray2} a interseção entre eles é: ${intersectionResult}`)
 
 // Atividade 26
-const averageArray = [10,0,10];
+const averageArray = [10, 0, 10];
 
 console.log(`O array 1 é ${averageArray}, a média dos itens dele é: ${arrayAverage(averageArray)}`)
+
+// Atividade 27
+console.log(`O objeto é ${objetExample}, a soma das props é: ${sumProperties(objetExample)}`)
+
+// Atividade 28
+const objtFiltrado = filterProperties(filterArray, completeObj)
+console.log(`O objeto filtrado é:\n`)
+console.log(objtFiltrado)
