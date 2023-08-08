fd2991814459c6eb46d511bd858d3a79df66376f
Marcos <araujo678@gmail.com>
2023-08-08 12:52:57 -0300

    Atividade 33

--- 4_DOM/js/scripts.js
+++ 4_DOM/js/scripts.js
@@ -1,2 +1,31 @@
 //30 - Selecionar elemento e alterar
-document.getElementById("titulo").innerText = "Novo titulo!"
+document.getElementById("titulo").innerText = "Novo titulo!";
+
+//31 - Alterando CSS
+document.querySelectorAll('p').forEach(e => e.style.color = "red");
+
+//32 - Alterando classe
+const activeColor = () =>{
+    const divContainer = document.querySelector(".div-container");
+    divContainer.classList.toggle("ativo");
+}
+
+const btn = document.querySelector("#color-button");
+
+btn.addEventListener("click", () => {
+    activeColor();
+});
+
+//33 - Adicionando elementos pelo DOM
+const addLi = () => {
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("New item by click!"));
+    ul.appendChild(li);
+}
+
+const addBtn = document.querySelector("#new-item-btn");
+
+addBtn.addEventListener("click", () => {
+    addLi();
+});
